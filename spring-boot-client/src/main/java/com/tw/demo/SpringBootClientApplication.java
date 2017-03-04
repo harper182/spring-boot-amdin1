05ad0d48e89b532c8765eb4ec210874ebfe4e7a9
Haibo Wang <982255390@qq.com>
2017-03-05 01:02:14 +0800

    init project

---
+++ spring-boot-client/src/main/java/com/tw/demo/SpringBootClientApplication.java
@@ -0,0 +1,24 @@
+package com.tw.demo;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+public class SpringBootClientApplication {
+//    @Bean
+//    public Config hazelcastConfig() {
+//        return new Config().setProperty("hazelcast.jmx", "true")
+//                .addMapConfig(new MapConfig("spring-boot-admin-application-store").setBackupCount(1)
+//                        .setEvictionPolicy(EvictionPolicy.NONE))
+//                .addListConfig(new ListConfig("spring-boot-admin-event-store").setBackupCount(1)
+//                        .setMaxSize(1000));
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootClientApplication.class, args);
+    }
+}
